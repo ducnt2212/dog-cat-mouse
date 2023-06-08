cb7ef608892b4e3febc562e36a13de0ca7dfe2cc
ducnt2212 <ducnt2212@gmail.com>
2023-06-08 15:16:29 +0700

    Improve cat eat method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-import Cat from "./Cat";
+import Cat from "./Cat.js";
+import Mouse from "./Mouse.js";
+import Dog from "./Dog.js";
+
+var cat = new Cat();
+var mouse = new Mouse("Mickey");
+var dog = new Dog();
+try {
+  cat.eat(dog);
+} catch (error) {
+  console.log("Error while cat eating a dog");
+}
+console.log(cat);
